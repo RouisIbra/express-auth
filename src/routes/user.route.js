65b927e7ba867b9314d3e4dc64b6aa4aef909711
RouisIbra <123466397+RouisIbra@users.noreply.github.com>
2023-01-26 14:48:41 +0100

    show not logged in message

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,8 +3,12 @@ const authRequired = require("../middlewares/auth-required");
 
 const userRouter = express.Router();
 
-userRouter.get("/", authRequired(true), (req, res) => {
-  res.status(200).json(req.session.user);
+userRouter.get("/", (req, res) => {
+  if (req.session.user) {
+    res.status(200).json(req.session.user);
+  } else {
+    res.status(401).json({ message: "You are not logged in" });
+  }
 });
 
 module.exports = userRouter;
